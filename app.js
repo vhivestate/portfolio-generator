2f3f069c0018def5b3f25cfe086937a69edbadaf
vhivestate <garcia.valeria001@gmail.com>
2022-05-30 19:17:58 -0600

    add about section

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile('./index.html', pageHTML, err => {
 //   if (err) throw err;
 
-//   console.log('Portfolio complete! Check out index.html to see the output!');
+  // console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -18,12 +19,11 @@ const promptUser = () => {
       validate: nameInput => {
         if (nameInput) {
           return true;
-        } else {
+        } else { 
           console.log('Please enter your name!');
           return false;
         }
       }
-
     },
     {
       type: 'input',
@@ -145,5 +145,11 @@ if (!portfolioData.projects) {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
